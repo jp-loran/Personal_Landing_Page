49cb0bdf37bea99afa568ccf39794ea7ce8e7cc6
Juan Pablo Alvarez Loran <jpalvarezloran@comunidad.unam.mx>
2021-08-25 01:28:52 -0500

    Parallax effect added

---
+++ js/parallax-bg.js
@@ -0,0 +1,8 @@
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+  this.querySelectorAll('.layer').forEach(layer => {const speed = layer.getAttribute('data-speed')
+  const x=(window.innerWidth-e.pageX*speed)/400
+  const y=(window.innerHeight-e.pageY*speed)/200
+  layer.style.transform=`translateX(${x}px) translateY(${y}px)`
+})
+}
